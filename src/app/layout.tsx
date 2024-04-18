b66f5b74699fa7020776664be5dfea35d5d086ec
Michal Lata <michallata.dev@gmail.com>
2024-04-18 19:42:58 +0200

    patch: Update RootLayout styles and add background pattern

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,8 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body
-				className={`${poppins.className} bg-gradient-background bg-black text-white antialiased`}
-			>
+			<body className={`${poppins.className} bg-gradient-background text-white antialiased`}>
+				<div className="absolute -z-10 h-full w-full bg-[radial-gradient(#e5e7eb14_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]" />
 				{children}
 			</body>
 		</html>
