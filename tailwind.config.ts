edc716e9f20af12a4f0f09c45f6135a3dfef8563
Michal Lata <michallata.dev@gmail.com>
2024-04-19 12:13:00 +0200

    feat: Add fade animation in tailwind cfg

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,63 @@ const config: Config = {
 			black: "#222222",
 		},
 		extend: {
+			animation: {
+				"fade-up": "fade-up 1s ease-out both",
+				"fade-down": "fade-down 1s ease-out both",
+				"fade-left": "fade-left 1s ease-out both",
+				"fade-right": "fade-right 1s ease-out both",
+				"fade-in": "fade-in 1s ease-out both",
+			},
+			keyframes: {
+				"fade-up": {
+					"0%": {
+						opacity: "0",
+						transform: "translateY(1rem)",
+					},
+					"100%": {
+						opacity: "1",
+						transform: "translateY(0)",
+					},
+				},
+				"fade-down": {
+					"0%": {
+						opacity: "0",
+						transform: "translateY(-1rem)",
+					},
+					"100%": {
+						opacity: "1",
+						transform: "translateY(0)",
+					},
+				},
+				"fade-left": {
+					"0%": {
+						opacity: "0",
+						transform: "translateX(1rem)",
+					},
+					"100%": {
+						opacity: "1",
+						transform: "translateX(0)",
+					},
+				},
+				"fade-right": {
+					"0%": {
+						opacity: "0",
+						transform: "translateX(-1rem)",
+					},
+					"100%": {
+						opacity: "1",
+						transform: "translateX(0)",
+					},
+				},
+				"fade-in": {
+					"0%": {
+						opacity: "0",
+					},
+					"100%": {
+						opacity: "1",
+					},
+				},
+			},
 			colors: {
 				primary: "#42880C",
 				"primary-dark": "#336A0A",
