d2e2ffc0a96280cfed2eaa5687a795661ff11010
Michal Lata <michallata.dev@gmail.com>
2024-07-12 19:15:37 +0200

    feat: Add PortfolioList component

---
+++ src/components/portfolio/PortfolioList.tsx
@@ -0,0 +1,12 @@
+import { PortfolioListItem } from "@/components/portfolio/PortfolioListItem";
+import { PORTFOLIO_ITEMS } from "@/constants";
+
+export const PortfolioList = () => {
+	return (
+		<ul className="flex flex-col gap-8">
+			{PORTFOLIO_ITEMS.map((item) => (
+				<PortfolioListItem key={item.title} projectDetails={item} />
+			))}
+		</ul>
+	);
+};
