f4e010021c8b6d56d486dba8a7b46bd7095ace1e
Michal Lata <michallata.dev@gmail.com>
2024-07-13 13:03:38 +0200

    patch: Update PortfolioPage styles

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -5,8 +5,8 @@ import { PortfolioList } from "@/components/portfolio/PortfolioList";
 
 export default function PortfolioPage() {
 	return (
-		<main className="flex min-h-dvh items-center justify-start gap-6 px-4">
-			<section className="container flex flex-col gap-8">
+		<main className="container mx-auto flex items-center justify-start gap-6 px-4">
+			<section className="flex flex-col gap-8">
 				<header className="flex flex-col gap-2 border-l-8 border-primary pl-2">
 					<Link href="/" className="flex w-fit gap-2">
 						<ArrowLeftCircleIcon />
