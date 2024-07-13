8b2ab77e2a99d8522bdf6d90cf3cab1efc3080a5
Michal Lata <michallata.dev@gmail.com>
2024-07-13 13:02:41 +0200

    feat: Add ButtonLink component

---
+++ src/components/ButtonLink.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+type ButtonLinkProps = {
+	href: string;
+	icon: React.ReactNode;
+	label: string;
+};
+
+export const ButtonLink = ({ href, icon, label }: ButtonLinkProps) => {
+	return (
+		<Link
+			href={href}
+			target="_blank"
+			rel="noopener noreferrer"
+			className="flex gap-2 rounded-sm bg-primary p-4 shadow-sm transition-colors hover:bg-primary-dark"
+		>
+			{icon}
+			<span className="sr-only sm:not-sr-only">{label}</span>
+		</Link>
+	);
+};
