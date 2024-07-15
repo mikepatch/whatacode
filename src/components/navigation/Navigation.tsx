693d817762cb726d032e3ac05a1a8da440ba74d9
Michal Lata <michallata.dev@gmail.com>
2024-07-15 17:45:21 +0200

    patch: Update Navigation component

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,17 +1,20 @@
 "use client";
 
 import { useState } from "react";
-import { Navbar } from "@/components/navigation/Navbar";
-import { Sidebar } from "@/components/navigation/Sidebar";
+
+import { NavDesktop } from "@/components/navigation/NavDesktop";
+import { NavMobile } from "@/components/navigation/NavMobile";
+import { NavToggleButton } from "@/components/navigation/NavToggleButton";
 
 export const Navigation = () => {
 	const [isOpen, setIsOpen] = useState(false);
 	const toggle = () => setIsOpen((prev) => !prev);
 
 	return (
-		<>
-			<Sidebar isOpen={isOpen} toggle={toggle} />
-			<Navbar toggle={toggle} />
-		</>
+		<nav className="container sticky top-0 flex h-20 w-full items-center justify-end px-4">
+			<NavMobile isOpen={isOpen} toggle={toggle} />
+			<NavDesktop />
+			<NavToggleButton isOpen={isOpen} toggle={toggle} />
+		</nav>
 	);
 };
