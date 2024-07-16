8348c4532e1d936ccf5d990232a1ba091de79590
Michal Lata <michallata.dev@gmail.com>
2024-07-16 17:26:20 +0200

    feat: Add consts

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,6 +1,6 @@
 import { Github, Linkedin, Mail } from "lucide-react";
 
-import { type TSocialLink } from "@/types";
+import { type TPortfolioItem, type TNavItem, type TSocialLink } from "@/types";
 
 export const SOCIAL_LINKS: TSocialLink[] = [
 	{
@@ -17,5 +17,59 @@ export const SOCIAL_LINKS: TSocialLink[] = [
 		href: "mailto:michallata.dev@gmail.com",
 		icon: <Mail />,
 		label: "Email",
+	},
+];
+
+export const NAV_ITEMS: TNavItem[] = [
+	{
+		href: "/",
+		label: "home",
+	},
+	{
+		href: "/about",
+		label: "about",
+	},
+	{
+		href: "/portfolio",
+		label: "portfolio",
+	},
+];
+
+export const PORTFOLIO_ITEMS: TPortfolioItem[] = [
+	{
+		title: "Sala Vita",
+		technologies: ["TypeScript", "React", "Next.js"],
+		description: "The website project for my customer.",
+		links: [
+			{
+				href: "https://sala-vita.pl",
+				label: "Live demo",
+			},
+			{
+				href: "https://github.com/mikepatch/sala-vita--frontend",
+				label: "Front-end code",
+			},
+		],
+		imageSrc: "/images/portfolio/portfolio-sala-vita.png",
+	},
+	{
+		title: "RevvRush e-commerce",
+		technologies: ["TypeScript", "React", "Next.js", "NestJS", "GraphQL"],
+		description: "The e-commerce site project to practice front-end and back-end skills.",
+		links: [
+			{
+				href: "https://revvrush.vercel.app",
+				label: "Live demo",
+			},
+			{
+				href: "https://github.com/mikepatch/revvrush-ecommerce--frontend",
+				label: "Front-end code",
+			},
+			{
+				href: "https://github.com/mikepatch/revvrush-ecommerce--backend",
+				label: "Back-end code",
+			},
+		],
+		imageSrc: "/images/portfolio/portfolio-revvrush-ecommerce.png",
 	},
 ];
