843e5c55c05746f6621c3b0563523b588d459afe
Michal Lata <michallata.dev@gmail.com>
2024-07-16 17:26:02 +0200

    feat: Add types

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,21 @@ export type TSocialLink = {
 	icon: React.ReactNode;
 	label: string;
 };
+
+export type TNavItem = {
+	href: string;
+	label: string;
+};
+
+export type TPortfolioLink = {
+	href: string;
+	label: string;
+};
+
+export type TPortfolioItem = {
+	title: string;
+	description: string;
+	technologies: string[];
+	imageSrc: string;
+	links: TPortfolioLink[];
+};
