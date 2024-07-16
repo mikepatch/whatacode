a48a2434d35fd95b7f196ad7fdb3f928f0df6ff9
Michal Lata <michallata.dev@gmail.com>
2024-07-16 17:22:50 +0200

    patch: Update NavMobile component

--- src/components/navigation/NavMobile.tsx
+++ src/components/navigation/NavMobile.tsx
@@ -12,27 +12,33 @@ export const NavMobile = ({ isOpen, toggle }: MobileProps) => {
 	return (
 		<aside
 			className={clsx(
-				"fixed inset-0 flex bg-black bg-opacity-70 bg-clip-padding backdrop-blur-md backdrop-filter transition-all duration-300 ease-in-out",
+				"fixed inset-0 flex items-center bg-black bg-opacity-70 bg-clip-padding backdrop-blur-md backdrop-filter transition-all duration-300 ease-in-out",
 				isOpen
 					? "pointer-events-auto visible opacity-100"
 					: "pointer-events-none invisible opacity-0",
 			)}
 		>
 			<div className="absolute inset-0" onClick={toggle} />
-			<ul className="relative z-10 flex flex-col justify-center gap-6 overflow-hidden text-4xl">
-				{NAV_ITEMS.map((item) => (
-					<li key={item.href} className="w-full">
-						<ActiveLink
-							className="border-b-2 border-primary border-opacity-0 py-1 pl-6 pr-2 transition-all hover:border-opacity-100"
-							href={item.href}
-							activeClassName="!border-opacity-100"
-							onClick={toggle}
+			{isOpen && (
+				<ul className="relative z-10 flex flex-col gap-8 text-5xl">
+					{NAV_ITEMS.map((item, index) => (
+						<li
+							key={item.href}
+							className="w-full animate-fade-up pl-6"
+							style={{ animationDelay: `${index * 200}ms` }}
 						>
-							{item.label}
-						</ActiveLink>
-					</li>
-				))}
-			</ul>
+							<ActiveLink
+								className="border-l-8 border-primary border-opacity-0 pl-2 transition-all  hover:border-opacity-100"
+								href={item.href}
+								activeClassName="!border-opacity-100"
+								onClick={toggle}
+							>
+								{item.label}
+							</ActiveLink>
+						</li>
+					))}
+				</ul>
+			)}
 		</aside>
 	);
 };
