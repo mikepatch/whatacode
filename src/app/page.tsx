5f2d6f338c6788c4027c0911e320e55a323eed66
Michal Lata <michallata.dev@gmail.com>
2024-07-16 17:27:40 +0200

    patch: Update HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,17 @@
 import NextImage from "next/image";
-import Link from "next/link";
 
 import { SOCIAL_LINKS } from "@/constants";
+import { ButtonLink } from "@/components/ButtonLink";
 
 export default function Home() {
 	return (
-		<main className="flex h-dvh items-center justify-center">
-			<section className="flex flex-col items-center gap-8">
-				<header className="flex flex-col items-center gap-2">
-					<h1 className="text-6xl font-bold">Michał Łata</h1>
+		<main className="-mt-20 flex h-dvh items-center justify-center gap-6 px-2">
+			<section className="flex flex-col gap-8">
+				<header className="flex flex-col gap-2 border-l-8 border-primary pl-2">
+					<h1 className="text-5xl font-bold sm:text-6xl">Michał Łata</h1>
 					<p className="">Web Developer | Tech & AI enthusiast</p>
-					<ul className="flex gap-2 text-xl font-bold text-primary">
-						<li className="animation-delay-200 animate-fade-right">TypeScript</li>
-						<li className="animation-delay-550 animate-fade-down">React</li>
-						<li className="animation-delay-750 animate-fade-down">Next.js</li>
-						<li className="animation-delay-1100 animate-fade-left">NestJS</li>
-					</ul>
 				</header>
-				<figure className="outline-brand-primary aspect-square w-56 overflow-hidden rounded-full shadow-sm outline">
+				<figure className="aspect-square w-56 self-center overflow-hidden rounded-full border-4 border-white shadow-sm">
 					<NextImage
 						src="/images/mike.jpeg"
 						alt="Michał Łata – Web Developer"
@@ -27,20 +21,18 @@ export default function Home() {
 						loading="eager"
 					/>
 				</figure>
+				<ul className="-z-10 flex justify-center gap-2 text-xl font-bold text-primary">
+					<li className="animate-fade-right animation-delay-200">TypeScript</li>
+					<li className="animate-fade-down animation-delay-550">React</li>
+					<li className="animate-fade-down animation-delay-750">Next.js</li>
+					<li className="animate-fade-left animation-delay-1100">NestJS</li>
+				</ul>
 				<article className="flex flex-col gap-4">
-					You can find me on:
-					<ul className="flex gap-6">
+					<p>You can find me on:</p>
+					<ul className="flex justify-evenly gap-2 sm:gap-4">
 						{SOCIAL_LINKS.map(({ href, icon, label }) => (
 							<li key={href}>
-								<Link
-									href={href}
-									target="_blank"
-									rel="noopener noreferrer"
-									className="block rounded-md bg-primary p-4 shadow-sm transition-colors hover:bg-primary-dark"
-								>
-									<span className="sr-only">{label}</span>
-									{icon}
-								</Link>
+								<ButtonLink href={href} icon={icon} label={label} />
 							</li>
 						))}
 					</ul>
