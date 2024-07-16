070965b65023699db28f3b4dc07c7a9d591f0f3c
Michal Lata <michallata.dev@gmail.com>
2024-07-16 17:24:46 +0200

    feat: Add scrolling logic in Navigation component for background color

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,17 +1,38 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import clsx from "clsx";
 
 import { NavDesktop } from "@/components/navigation/NavDesktop";
 import { NavMobile } from "@/components/navigation/NavMobile";
 import { NavToggleButton } from "@/components/navigation/NavToggleButton";
 
 export const Navigation = () => {
+	const [scrolled, setScrolled] = useState(false);
 	const [isOpen, setIsOpen] = useState(false);
 	const toggle = () => setIsOpen((prev) => !prev);
 
+	const handleScroll = () => {
+		if (window.scrollY > 20) {
+			setScrolled(true);
+		} else {
+			setScrolled(false);
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, []);
+
 	return (
-		<nav className="container sticky top-0 flex h-20 w-full items-center justify-end px-4">
+		<nav className="sticky top-0 flex h-20 w-full items-center justify-end px-4">
+			<div
+				className={clsx("absolute inset-0 -mt-px transition-colors", {
+					"bg-gradient-background shadow-lg": scrolled,
+				})}
+			/>
 			<NavMobile isOpen={isOpen} toggle={toggle} />
 			<NavDesktop />
 			<NavToggleButton isOpen={isOpen} toggle={toggle} />
