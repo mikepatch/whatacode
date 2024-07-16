172fa17d61fab87391f222fdad670c581b6f0710
Michal Lata <michallata.dev@gmail.com>
2024-07-16 17:27:24 +0200

    patch: Update styles and text in PortfolioListItem

--- src/components/portfolio/PortfolioListItem.tsx
+++ src/components/portfolio/PortfolioListItem.tsx
@@ -1,5 +1,6 @@
 import NextImage from "next/image";
 import Link from "next/link";
+
 import { type TPortfolioItem } from "@/types";
 
 type PortfolioListItemProps = {
@@ -10,7 +11,7 @@ export const PortfolioListItem = ({
 	projectDetails: { title, technologies, description, links, imageSrc },
 }: PortfolioListItemProps) => {
 	return (
-		<li className="grid gap-4 sm:grid-cols-3">
+		<li className="flex flex-col gap-4 sm:grid sm:grid-cols-3">
 			<figure className="aspect-video w-full self-center overflow-hidden rounded-sm shadow-sm">
 				<NextImage
 					src={imageSrc}
@@ -24,9 +25,13 @@ export const PortfolioListItem = ({
 			<article className="flex flex-col justify-between gap-2 sm:col-span-2">
 				<header>
 					<h2 className="text-2xl font-bold">{title}</h2>
-					<ul className="flex gap-2 text-lg text-primary">
-						{technologies.map((technology) => (
-							<li key={technology} className="animate-fade-right animation-delay-200">
+					<ul className="flex flex-wrap gap-2 text-lg text-primary">
+						{technologies.map((technology, index) => (
+							<li
+								key={technology}
+								className="-z-10 animate-fade-up"
+								style={{ animationDelay: `${index * 200}ms` }}
+							>
 								{technology}
 							</li>
 						))}
@@ -43,7 +48,7 @@ export const PortfolioListItem = ({
 									referrerPolicy="no-referrer"
 									className="font-bold text-primary underline transition-colors hover:text-primary-dark"
 								>
-									click to view
+									Link
 								</Link>
 							</li>
 						))}
