2a6d72f486160e4f4bb1804d3a91568f15c0c5e5
Michal Lata <michallata.dev@gmail.com>
2024-07-17 12:04:30 +0200

    feat: Remove go back button from PortfolioPage

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,6 +1,3 @@
-import { ArrowLeftCircleIcon } from "lucide-react";
-import Link from "next/link";
-
 import { PortfolioList } from "@/components/portfolio/PortfolioList";
 
 export default function PortfolioPage() {
@@ -8,10 +5,6 @@ export default function PortfolioPage() {
 		<main className="container mx-auto flex items-center justify-start gap-6 px-4">
 			<section className="flex flex-col gap-8">
 				<header className="flex flex-col gap-2 border-l-8 border-primary pl-2">
-					<Link href="/" className="flex w-fit gap-2">
-						<ArrowLeftCircleIcon />
-						<span>go back</span>
-					</Link>
 					<h1 className="text-5xl font-bold sm:text-6xl">my portfolio</h1>
 				</header>
 				<PortfolioList />
