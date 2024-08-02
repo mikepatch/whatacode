28e7a7d570c2fc435d3dfdaaa87c5455b552bc91
Michal Lata <michallata.dev@gmail.com>
2024-08-02 13:20:25 +0200

    patch: Update bg color in Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
 	return (
-		<footer className="flex h-20 w-full items-center justify-center bg-primary text-white">
+		<footer className="flex h-20 w-full items-center justify-center bg-primary-dark text-white">
 			<p>© 2024 - Michał Łata | whatacode.pl</p>
 		</footer>
 	);
