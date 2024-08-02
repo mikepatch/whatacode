46bac26d9e513c7c4ac1ae63237d766050298cc4
Michal Lata <michallata.dev@gmail.com>
2024-08-02 13:18:39 +0200

    patch: Update spacing in PageContainer

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -5,8 +5,8 @@ type PageContainerProps = {
 
 export const PageContainer = ({ title, children }: PageContainerProps) => {
 	return (
-		<main className="container mx-auto flex max-w-5xl flex-col gap-8 px-4 py-40">
-			<header className="flex flex-col gap-2">
+		<main className="container mx-auto flex max-w-5xl flex-col gap-12 px-4 py-40">
+			<header>
 				<h1 className="border-l-8 border-primary pl-2 text-5xl font-bold sm:text-6xl">{title}</h1>
 			</header>
 			{children}
